40efbbc9668e1b29e2191f70eed09103418c3a35
Vincent Tavernier <vince.tavernier@gmail.com>
2017-09-22 17:24:29 +0200

    Clean user devise routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :trackable, :validatable
 
   validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :registrations
+  devise_scope :user do
+    resource :registration,
+             only: [:edit, :update],
+             path: 'users',
+             controller: 'devise/registrations',
+             as: :user_registration
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
