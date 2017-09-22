36b3b7fe66a4f35ceb266969f31dac5df20c4d89
Vincent Tavernier <vince.tavernier@gmail.com>
2017-09-22 13:52:56 +0200

    Fix redirection on submission delete

--- app/admin/submission.rb
+++ app/admin/submission.rb
@@ -3,5 +3,11 @@ ActiveAdmin.register Submission do
 
   controller do
     actions :destroy
+
+    def destroy
+      destroy! do |format|
+        format.html { redirect_to admin_project_path(resource.project) }
+      end
+    end
   end
 end
