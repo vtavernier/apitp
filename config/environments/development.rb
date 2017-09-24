e8fb2024f7fefe24a24868516b028e3ad3cbecc9
Vincent Tavernier <vince.tavernier@gmail.com>
2017-09-24 13:42:40 +0200

    Setup smtp4dev settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Use local SMTP for dev
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'localhost',
+      port: 2525,
+      domain: 'localhost',
+      enable_starttls_auto: false
+  }
 end
