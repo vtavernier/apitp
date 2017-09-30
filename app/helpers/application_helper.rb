ec8f379d20905d5dc069ac3e0bbd53f1252de76d
Vincent Tavernier <vince.tavernier@gmail.com>
2017-09-30 16:59:50 +0200

    Fix navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     content_for(:title, page_title)
     return page_title
   end
+
+  def navbar_element(name, link)
+    if current_page? link
+      ('<li class="active">' + link_to(name, '#') + '</li>').html_safe
+    else
+      ('<li>' + link_to(name, link) + '</li>').html_safe
+    end
+  end
 end
